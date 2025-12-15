027d681116f6b70e40aef9ad4a2792b54a2620c7
Niels Taes <nielstaes07@gmail.com>
2025-12-15 16:06:31 +0100

    python bestanden Tove

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import pygame
+
+
 def main():
     ...
 
 
 def create_main_surface():
     pygame.init()
-
+    klok = pygame.time.Clock()
+    #hey niels hoe gaat het?
     screen_size = (1024, 768)
 
     screen=pygame.display.set_mode(screen_size)
     pygame.draw.circle(screen, (255,100,255), (10,10), 100)
     pygame.display.flip()
+    
 while True:
     create_main_surface()
 
