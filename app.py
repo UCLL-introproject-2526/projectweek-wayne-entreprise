d136792b22957d90a2f598c8c6eb29e61b371417
Niels Taes <nielstaes07@gmail.com>
2025-12-15 16:53:46 +0100

    test maandag

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import pygame
-
-
 def main():
     ...
 
@@ -8,13 +6,38 @@ def main():
 def create_main_surface():
     pygame.init()
     klok = pygame.time.Clock()
-    #hey niels hoe gaat het?
-    screen_size = (1024, 768)
+    pygame.display.set_caption("Kerst")
 
+    screen_size = (1024, 768)
     screen=pygame.display.set_mode(screen_size)
-    pygame.draw.circle(screen, (255,100,255), (10,10), 100)
-    pygame.display.flip()
-    
-while True:
-    create_main_surface()
+    running = True
+    position = 0
+    move_x = 0
+    move_y=0
+    while running:
+        pygame.draw.circle(screen, (255,100,255), (position,200), 20)
+        pygame.display.flip()
+        screen.fill((0,0,0))
+        klok.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_LEFT:
+                    move_x = -2
+                if event.key == pygame.K_RIGHT:
+                    move_x = 2
+                if event.key == pygame.K_SPACE:
+                    move_y +=2
+        position += move_x
+        if position < 0:
+            position = 0
+        if position > 1024:
+            position = 1024
+    pygame.quit()
+
+
+create_main_surface()
 
