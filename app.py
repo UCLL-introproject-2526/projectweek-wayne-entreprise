91fbf38c9f7f598b4f02ffbd7df915c1a6021a76
Niels Taes <nielstaes07@gmail.com>
2025-12-16 14:32:18 +0100

    tussen

--- app.py
+++ app.py
@@ -53,10 +53,12 @@ def game_loop():
     screen = pygame.display.set_mode((320,320), pygame.FULLSCREEN | pygame.SCALED)
 
     c1 = character.Character((0, 160), 10)
+    
     g1 = goal.Goal(screen)
     move_left = False
     move_right = False
     while running:
+        c1_hitbox = pygame.Rect(c1.x, c1.y, c1.idle_pose.get_width(), c1.idle_pose.get_height())
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -81,16 +83,28 @@ def game_loop():
             c1.move_right()
         else:
             ...
-            
+
         screen.blit(background, (0,0))
         screen.blit(c1.idle_pose, (c1.x, c1.y))
-        rectangle = pygame.rect.Rect(150, 50, 32, 32)
+        rectangle = pygame.rect.Rect(150, 160, 32, 32)
         pygame.draw.rect(screen, (255, 0, 0), rectangle)
+        if c1_hitbox.colliderect(rectangle):
+            running = g1.win()
+
     
 
 
         klok.tick(60)
         pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        klok.tick(60)
     pygame.quit()
 
 game_loop()
--- goal.py
+++ goal.py
@@ -4,12 +4,13 @@ class Goal:
     def __init__(self, scherm):
         self.image = None
         self.screen = scherm
-        self.place()
         
     def win(self):
-        pygame.font.Font("aria", 32)
-        text = pygame.font.render("You delivered all the presents!!!", True, (0, 255, 0), background=None)
-        self.screen.blit(text, ((320-text.get_witdh())//2, (320-text.get_height())//2))
+        font = pygame.font.Font('freesansbold.ttf', 16)
+        text = font.render("You delivered all the presents!!!", True, (0, 255, 0))
+        text_width, text_height = font.size("You delivered all the presents!!!")
+        self.screen.blit(text, ((320-text_width)//2, (320-text_height)//2))
+        return False
     
     def place(self):
         """
