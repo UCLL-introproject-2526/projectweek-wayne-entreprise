6a256fb2d5bf717b073aec337ff02a62e3089c96
Blopert.Seppe <Blopert.seppemiseur@outlook.com>
2025-12-16 11:56:55 +0100

    Color package change

--- package.py
+++ package.py
@@ -1,12 +1,27 @@
 import pygame
 
+from random import randint
+
 class Package:
     def __init__(self):
         self.x = 0
         self.y = -100
+        set_color()
+
+    
+    def set_color(self):
+        rand = randint(1,3)
+        if rand == 1:
+            self.color = (230,33,33) #red
+        elif rand == 2:
+            self.color = (33,230,33) #green
+        else:
+            self.color = (33,33,230) #blue
     
     def check_for_collission(self, place_coördinates):
         ...
     
     def place():
         ...
+
+p = Package()
