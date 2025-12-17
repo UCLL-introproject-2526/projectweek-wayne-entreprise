fc47a7c89a37b84105b04913517b8993baea5af1
Blopert.Seppe <Blopert.seppemiseur@outlook.com>
2025-12-17 09:21:37 +0100

    package placing begin

--- character.py
+++ character.py
@@ -14,7 +14,7 @@ class Character:
         self.y = position_y_platform
 
     def playerfalling(self, dt):
-        gravity = 0.0005
+        gravity = 0.0010
         self.speed_y += gravity * dt
         increase = self.speed_y * dt
         self.y  += increase
